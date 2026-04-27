1ca836e7717766f3b6e41579e18bd460575798af
alba-x-ai <j4albaai@gmail.com>
2026-04-27 18:01:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const tg = window.Telegram.WebApp
 tg.ready()
 
+const menuDiv = document.getElementById("menu")
+const cartDiv = document.getElementById("cart")
+
 let cart = []
 let currentMenu = null
 let currentType = null
 let currentSelection = {}
-
-const menuDiv = document.getElementById("menu")
-const cartDiv = document.getElementById("cart")
 
 // --- ДОПЫ ---
 const breadOptions = [
@@ -26,113 +26,54 @@ const pieOptions = [
 const menus = {
 
 russian: [
-
 {
 day:"Понедельник",
-fixed:[
-"Салат «Летний»",
-"Гречка с грибами и луком",
-"Солёные огурцы"
-],
-soups:[
-"Борщ + Гороховый суп",
-"Куриный суп с сухариками + Рассольник"
-],
+fixed:["Салат «Летний»","Гречка с грибами и луком","Солёные огурцы"],
+soups:["Борщ + Гороховый суп","Куриный суп с сухариками + Рассольник"],
 mains:[
 "Говядина тушёная с картофельным пюре + Куриная печень с рисом",
 "Котлета из говядины с гречкой + Котлета куриная с запечёнными овощами"
 ]
 },
-
 {
 day:"Среда",
-fixed:[
-"Свекольный салат с орехом, чесноком, изюмом",
-"Перловая каша с грибами и луком",
-"Квашеная капуста"
-],
-soups:[
-"Куриный суп с сухариками + Рассольник",
-"Солянка + Щи"
-],
+fixed:["Свекольный салат с орехом, чесноком, изюмом","Перловая каша с грибами и луком","Квашеная капуста"],
+soups:["Куриный суп с сухариками + Рассольник","Солянка + Щи"],
 mains:[
 "Котлета из говядины с гречкой + Котлета куриная с запечёнными овощами",
 "Курица запечённая с лапшой + Свиные рёбра с тушёной капустой"
 ]
 },
-
 {
 day:"Пятница",
-fixed:[
-"Салат «Витаминный»",
-"Гороховая каша с грибами и луком",
-"Маринованные помидоры"
-],
-soups:[
-"Борщ + Гороховый суп",
-"Солянка + Щи"
-],
+fixed:["Салат «Витаминный»","Гороховая каша с грибами и луком","Маринованные помидоры"],
+soups:["Борщ + Гороховый суп","Солянка + Щи"],
 mains:[
 "Говядина тушёная с картофельным пюре + Куриная печень с рисом",
 "Курица запечённая с лапшой + Свиные рёбра с тушёной капустой"
 ]
 }
-
 ],
 
 international: [
-
 {
 day:"Понедельник",
-fixed:[
-"Греческий салат",
-"Бутерброд с сыром",
-"Солёные огурцы"
-],
-soups:[
-"Шурпа + Крем-суп грибной",
-"Харчо + Овощной суп-пюре"
-],
-mains:[
-"Гуляш + Курица терияки",
-"Курица Кун Пао + Ломо сальтадо"
-]
+fixed:["Греческий салат","Бутерброд с сыром","Солёные огурцы"],
+soups:["Шурпа + Крем-суп грибной","Харчо + Овощной суп-пюре"],
+mains:["Гуляш + Курица терияки","Курица Кун Пао + Ломо сальтадо"]
 },
-
 {
 day:"Среда",
-fixed:[
-"Свекольный салат",
-"Бутерброд с сыром",
-"Квашеная капуста"
-],
-soups:[
-"Харчо + Овощной суп-пюре",
-"Lohikeitto + Яично-помидорный"
-],
-mains:[
-"Курица Кун Пао + Ломо сальтадо",
-"Кёнигсбергские клопсы + Индийское карри"
-]
+fixed:["Свекольный салат","Бутерброд с сыром","Квашеная капуста"],
+soups:["Харчо + Овощной суп-пюре","Lohikeitto + Яично-помидорный"],
+mains:["Курица Кун Пао + Ломо сальтадо","Кёнигсбергские клопсы + Индийское карри"]
 },
-
 {
 day:"Пятница",
-fixed:[
-"Салат с фунчозой",
-"Бутерброд с сыром",
-"Маринованные помидоры"
-],
-soups:[
-"Шурпа + Крем-суп грибной",
-"Lohikeitto + Яично-помидорный"
-],
-mains:[
-"Гуляш + Курица терияки",
-"Кёнигсбергские клопсы + Индийское карри"
-]
+fixed:["Салат с фунчозой","Бутерброд с сыром","Маринованные помидоры"],
+soups:["Шурпа + Крем-суп грибной","Lohikeitto + Яично-помидорный"],
+mains:["Гуляш + Курица терияки","Кёнигсбергские клопсы + Индийское карри"]
 }
-
 ]
 
 }
@@ -158,20 +99,24 @@ function openMenu(type){
 currentType = type
 currentMenu = menus[type]
 
-menuDiv.innerHTML = ""
+menuDiv.innerHTML = `
+
+<div style="margin-bottom:15px">
+<button onclick="renderMenuTypes()">← Назад</button>
+</div>
+
+`
 
 currentMenu.forEach((set,i)=>{
 
 menuDiv.innerHTML += `
-<div>
+<div style="background:white;padding:15px;margin-bottom:10px;border-radius:10px">
 <h2>${set.day}</h2>
 <button onclick="openDay(${i})">Выбрать</button>
 </div>
 `
 
 })
-
-menuDiv.innerHTML += `<button onclick="renderMenuTypes()">← Назад</button>`
 
 }
 
@@ -182,6 +127,10 @@ const set = currentMenu[index]
 const saved = currentSelection[set.day] || {}
 
 menuDiv.innerHTML = `
+
+<div style="margin-bottom:15px">
+<button onclick="saveAndBack('${set.day}')">← Назад</button>
+</div>
 
 <h2>${set.day}</h2>
 
@@ -227,7 +176,6 @@ ${p}
 <textarea id="comment">${saved.comment || ""}</textarea>
 
 <button onclick="addToCart('${set.day}')">Добавить в корзину</button>
-<button onclick="saveAndBack('${set.day}')">← Назад</button>
 
 `
 
@@ -244,13 +192,7 @@ const pies = [...document.querySelectorAll('input[name="pies"]:checked')].map(i=
 
 const comment = document.getElementById("comment").value
 
-currentSelection[day] = {
-soup,
-main,
-bread:breads,
-pies:pies,
-comment
-}
+currentSelection[day] = {soup,main,bread:breads,pies:pies,comment}
 
 }
 
@@ -271,17 +213,14 @@ alert("Выберите суп и второе")
 return
 }
 
-cart.push({
-day,
-...sel
-})
+cart.push({day,...sel})
 
 renderCart()
 alert("Добавлено в корзину")
 
 }
 
-// --- УДАЛЕНИЕ ---
+// --- УДАЛИТЬ ---
 function removeItem(index){
 cart.splice(index,1)
 renderCart()
@@ -296,18 +235,15 @@ return
 }
 
 cartDiv.innerHTML = `
-
-<div style="position:fixed;bottom:0;background:white;width:100%;padding:10px;max-height:50%;overflow:auto">
+<div style="position:fixed;bottom:0;background:white;width:100%;padding:10px">
 
 <b>Корзина (${cart.length})</b>
 
 ${cart.map((i,index)=>`
 <div>
-<b>${i.day}</b><br>
+${i.day}<br>
 ${i.soup}<br>
 ${i.main}<br>
-${i.bread.length ? "Хлеб: "+i.bread.join(", ")+"<br>" : ""}
-${i.pies.length ? "Пирожки: "+i.pies.join(", ")+"<br>" : ""}
 <button onclick="removeItem(${index})">Удалить</button>
 </div>
 <hr>
@@ -316,7 +252,6 @@ ${i.pies.length ? "Пирожки: "+i.pies.join(", ")+"<br>" : ""}
 <button onclick="checkout()">Оформить заказ</button>
 
 </div>
-
 `
 
 }
